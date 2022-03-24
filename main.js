5f716a5b35ba42c7d2c8ccc92655cbc121879f25
FalakTheCoder <92030342+FalakTheCoder@users.noreply.github.com>
2022-03-24 17:27:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function setup(){
     canvas = createCanvas(600, 500);
@@ -6,10 +10,16 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
 }
 
 function draw(){
-    image(video, 600, 500)
+    image(video, 0, 0, 600, 500)
 }
 
 function preload(){
@@ -18,4 +28,20 @@ function preload(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Righ Wrist X = " + rightWristX + "Right Wrist Y = " + rightWristY);
+   }
 }
